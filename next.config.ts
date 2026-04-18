f78b306135ed635cea69c891a32c3deaec7837f4
Solution Provider <idreesfaiz63@gmail.com>
2026-04-18 12:46:57 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@sparticuz/chromium"],
+  outputFileTracingIncludes: {
+    "/api/student/[id]/report": ["./src/assets/fonts/**/*"],
+  },
 };
 
 export default nextConfig;
